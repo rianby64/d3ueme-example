fdd8e756a238d334655c8c6f11d68d271196a8d5
Andres Rios <rianby64@gmail.com>
2017-01-20 01:36:25 +0300

    Cover with test render function

--- src/enexup.js
+++ src/enexup.js
@@ -1,34 +1,35 @@
 (() => {
   'use strict';
   document.querySelector('button').addEventListener('click', () => {
-    render();
-  });
-
-  var svg = window.d3.select('svg');
-  render();
-
-  function rnd(num) {
-    return parseInt(num * Math.random() + 1, 10);
-  }
-
-  function render() {
     var array = new Array(rnd(20))
       .join()
       .split(',')
       .map(() => rnd(100));
 
+    render(array);
+  });
+
+  var svg = d3.select('svg');
+
+  function rnd(num) {
+    return parseInt(num * Math.random() + 1, 10);
+  }
+
+  function render(array) {
+    var array_ = array || [];
     var text = svg.selectAll('text')
-      .data(array);
+      .data(array_);
 
     text.attr('class', 'update');
 
     text.enter().append('text')
       .attr('class', 'enter')
       .merge(text)
-      .attr('class', 'merge')
       .text(d => d);
 
     text.exit()
       .attr('class', 'exit');
   }
+
+  window.render = render;
 })();
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,99 @@
 (() => {
   'use strict';
-  async_test(function() {
-    this.done();
-  }, 'First test');
+  var render = window.render;
+
+  test(() => {
+    var arrayEnter = [1, 2, 3];
+
+    render(arrayEnter);
+
+    Array.prototype
+      .slice
+      .call(document.querySelectorAll('.enter'))
+      .forEach((item, i) => {
+        assert_equals(item.textContent, arrayEnter[i].toString());
+        assert_true(item.classList.contains('enter'));
+        assert_equals(item.classList.length, 1);
+      });
+  }, 'Create elements by enter');
+
+  test(() => {
+    var arrayEnterUpdate = [5, 4, 3, 2];
+
+    render(arrayEnterUpdate);
+
+    var lastI = 0;
+    Array.prototype
+      .slice
+      .call(document.querySelectorAll('.update'))
+      .forEach((item, i) => {
+        assert_equals(item.textContent, arrayEnterUpdate[i].toString());
+        assert_true(item.classList.contains('update'));
+        assert_equals(item.classList.length, 1);
+        lastI = i + 1;
+      });
+
+    assert_equals(lastI, 3);
+
+    Array.prototype
+      .slice
+      .call(document.querySelectorAll('.enter'))
+      .forEach((item, i) => {
+        var i_ = i + lastI;
+        assert_equals(item.textContent, arrayEnterUpdate[i_].toString());
+        assert_true(item.classList.contains('enter'));
+        assert_equals(item.classList.length, 1);
+      });
+  }, 'Update and append elements');
+
+  test(() => {
+    var arrayEnterUpdate = [1, 2];
+    var arrayExit = [1, 2, 3, 2];
+
+    render(arrayEnterUpdate);
+
+    var lastI = 0;
+    Array.prototype
+      .slice
+      .call(document.querySelectorAll('.update'))
+      .forEach((item, i) => {
+        assert_equals(item.textContent, arrayEnterUpdate[i].toString());
+        assert_true(item.classList.contains('update'));
+        assert_equals(item.classList.length, 1);
+        lastI = i + 1;
+      });
+
+    assert_equals(lastI, arrayEnterUpdate.length);
+    assert_equals(document.querySelectorAll('.enter').length, 0);
+
+    Array.prototype
+      .slice
+      .call(document.querySelectorAll('.exit'))
+      .forEach((item, i) => {
+        var i_ = i + lastI;
+        assert_equals(item.textContent, arrayExit[i_].toString());
+        assert_true(item.classList.contains('exit'));
+        assert_equals(item.classList.length, 1);
+      });
+  }, 'Update and remove elements');
+
+  test(() => {
+    var arrayEnterUpdate = [];
+    var arrayExit = [1, 2, 3, 2];
+
+    render(arrayEnterUpdate);
+
+    assert_equals(document.querySelectorAll('.enter').length, 0);
+    assert_equals(document.querySelectorAll('.update').length, 0);
+
+    Array.prototype
+      .slice
+      .call(document.querySelectorAll('.exit'))
+      .forEach((item, i) => {
+        assert_equals(item.textContent, arrayExit[i].toString());
+        assert_true(item.classList.contains('exit'));
+        assert_equals(item.classList.length, 1);
+      });
+  }, 'Remove elements');
+
 })();
